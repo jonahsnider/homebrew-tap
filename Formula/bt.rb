a1c43e56dd69b6111fe50500f992afebba07858f
Jonah Snider <jonah@jonahsnider.com>
2026-06-09 18:04:58 -0700

    Add bt formula

---
+++ Formula/bt.rb
@@ -0,0 +1,27 @@
+class Bt < Formula
+  desc "Braintrust command-line interface"
+  homepage "https://www.braintrust.dev/docs/reference/cli"
+  url "https://github.com/braintrustdata/bt/releases/download/v0.11.1/source.tar.gz"
+  sha256 "c714644492fac230aa73daaef93ced6bc559413d608a060dc7c2f69e1f2d5758"
+  license "Apache-2.0"
+  head "https://github.com/braintrustdata/bt.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  conflicts_with "bootterm", because: "both install a `bt` executable"
+
+  def install
+    unless build.head?
+      ENV["BT_VERSION_STRING"] = version.to_s
+      ENV["BT_UPDATE_CHANNEL"] = "stable"
+    end
+
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match(/\Abt \d+\.\d+\.\d+/, shell_output("#{bin}/bt --version"))
+    assert_match "2026-05-14T03:01:58Z",
+      shell_output("#{bin}/bt util version to-time p07639577379371417602 --utc")
+  end
+end
