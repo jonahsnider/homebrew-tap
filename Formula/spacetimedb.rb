ae5cec7c41396d561c941f1018309aac3532f7c2
Jonah Snider <jonah@jonahsnider.com>
2026-06-10 22:31:07 -0700

    Add spacetimedb formula
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

---
+++ Formula/spacetimedb.rb
@@ -0,0 +1,34 @@
+class Spacetimedb < Formula
+  desc "Database that is also a server"
+  homepage "https://spacetimedb.com/"
+  url "https://github.com/clockworklabs/SpacetimeDB/archive/refs/tags/v2.4.1.tar.gz"
+  sha256 "084f1e9351f4d7f5f9dfa9c88b19599b12c02fba686428f3bb30bc0f119a0f8f"
+  license "BUSL-1.1"
+  head "https://github.com/clockworklabs/SpacetimeDB.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "rust" => :build
+
+  def install
+    # Use source-tree file discovery since the release archive lacks git metadata.
+    ENV["SPACETIMEDB_NIX_BUILD_GIT_COMMIT"] = "v#{version}"
+
+    system "cargo", "install", *std_cargo_args(path: "crates/cli")
+    mv bin/"spacetimedb-cli", bin/"spacetime"
+
+    system "cargo", "install", *std_cargo_args(path: "crates/standalone")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/spacetime --version")
+
+    system bin/"spacetime", "--root-dir", testpath/"root", "init", "hello",
+           "--lang", "rust", "--server-only", "--local", "--non-interactive"
+    assert_path_exists testpath/"hello/spacetimedb/Cargo.toml"
+    assert_match "spacetimedb", (testpath/"hello/spacetimedb/Cargo.toml").read
+  end
+end
