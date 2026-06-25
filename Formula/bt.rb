2aaf390a232db4b54d34dd70bf532790a68b946b
Anka <runner@sat12-bq165-918fab29-a185-49a3-a276-df9e883e718c-9288F09016C8.local>
2026-06-25 03:44:14 +0000

    bt 0.14.0

--- Formula/bt.rb
+++ Formula/bt.rb
@@ -1,8 +1,8 @@
 class Bt < Formula
   desc "Braintrust command-line interface"
   homepage "https://www.braintrust.dev/docs/reference/cli"
-  url "https://github.com/braintrustdata/bt/releases/download/v0.12.0/source.tar.gz"
-  sha256 "c064c2ecf77a5447db734be793be8b50d9bd6e491c4737eb3af8444ee85f0bbd"
+  url "https://github.com/braintrustdata/bt/releases/download/v0.14.0/source.tar.gz"
+  sha256 "4fa5cc21d8891c85e2fcbca966eb1b9b3715d36df9965fc14e72f3aa4dc91bab"
   license "Apache-2.0"
   head "https://github.com/braintrustdata/bt.git", branch: "main"
 
