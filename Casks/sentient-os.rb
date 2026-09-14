00ebb9a38711053b89fde227464c1b98181d95cb
Anka <runner@iad20-gt1025-bf64fdbd-d924-481d-8de4-183d0d265132-A6E544C29AD7.local>
2026-09-14 23:02:40 +0000

    sentient-os 1.3

--- Casks/sentient-os.rb
+++ Casks/sentient-os.rb
@@ -1,6 +1,6 @@
 cask "sentient-os" do
-  version "1.0"
-  sha256 "e8b64273ed5db09422e97b61251b98cfc45c3aee6791ac75b2ddffe0254909b8"
+  version "1.3"
+  sha256 "ab7c065f2dd728742dc2f42ef74815125c061ea3690eeb1893c312986e9ad246"
 
   url "https://github.com/Sentient-OS-Labs/sentient-os/releases/download/#{version}/SentientOS-#{version}.dmg"
   name "Sentient OS"
